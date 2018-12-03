22049085d99862d541051da6b6ed8ab2f6986580
davidKolesar <djkolesa@gmail.com>
2018-12-03 13:24:56 -0500

    David Kolesar has successfully added placement of mines into grid and is now checking if user steps on them.

--- mineSweeper/mineSweeper.js
+++ mineSweeper/mineSweeper.js
@@ -1,34 +1,39 @@
     var lastClicked;
-    var totalCells;
-    var mineLocations = []
+    var totalCells = 0;
+    var totalMines = 10;
+    var mineLocations = [];
     
-    var grid = clickableGrid(10,10,function(el,row,col,i){
-        console.log("You clicked on element:",el);
+    var grid = clickableGrid(10,10,function(el,row,col,i)
+    {
         console.log("You clicked on row:",row);
         console.log("You clicked on col:",col);
-        console.log("You clicked on item #:",i);
+        var step = (row * col);
+        isStepOnMine(step);
     
         el.className='clicked';
         if (lastClicked) lastClicked.className='';
         lastClicked = el;
     });
-    
+  
+    //creates grid in html
     document.body.appendChild(grid);
-         
+
+    //assigns mines
+    randomlyAssignMines(totalMines);
+
     //draw grid
-    function clickableGrid(rows, cols, callback ){
-        var totalRows;
-        var totalColumns;
+    function clickableGrid(rows, cols, callback)
+    {
         var i = 0;
         var grid = document.createElement('table');
         grid.className = 'grid';
         
-        for (var r = 0; r < rows; ++r){
+        for (var r = 0; r < rows; ++r)
+        {
             var tr = grid.appendChild(document.createElement('tr'));
-            totalRows++;
-            for (var c = 0; c < cols; ++c){
+            for (var c = 0; c < cols; ++c)
+            {
                 var cell = tr.appendChild(document.createElement('td'));
-                totalColumns++;
                 cell.addEventListener('click',(function(el,r,c,i){
                     return function(){
                         callback(el,r,c,i);
@@ -36,7 +41,7 @@
                 })(cell,r,c,i),false);
             }
         }
-        totalCells = (totalColumns * totalRows);
+        totalCells = (cols * rows);
         return grid;
     }
 
@@ -45,14 +50,30 @@
         return Math.floor((Math.random() * 1000) + 1) % totalCells;
     }
 
-    function randomlyAssignMines(mines)
+    function randomlyAssignMines(totalMines)
     {
         var i, minesPlanted = 0;
 
-        while (minesPlanted < mines)
+        while (minesPlanted < totalMines)
         {
          var currentmineLocation = getRandomGridSquare();
-         mineLocations.pop(currentmineLocation);
+         console.log("dropping mine on " + currentmineLocation);
+         mineLocations.push(currentmineLocation);
          minesPlanted++;
+        }
+    }
+
+    function isStepOnMine(step) {      
+
+            function contains(mineLocations, step) {
+                console.log("user stepped on " + step);
+                for (var i = 0; i < a.length; i++) {
+                    if (mineLocations[i] == step) {
+                        console.log(true);
+                        return true;
+                    }
+                }
+                console.log(false);
+                return false;
         }
     }
